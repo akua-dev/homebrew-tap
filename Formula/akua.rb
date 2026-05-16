9fed96828079d45ae5ce4a5975a5547c99d8af57
akua-tap-bumper <akua-tap-bumper@cnap-tech>
2026-05-16 15:07:12 +0000

    akua v0.8.10
    Released in https://github.com/cnap-tech/akua/releases/tag/v0.8.10

--- Formula/akua.rb
+++ Formula/akua.rb
@@ -5,28 +5,28 @@
 class Akua < Formula
   desc "Cloud-native package build, transform, and preview toolkit"
   homepage "https://github.com/cnap-tech/akua"
-  version "0.8.8"
+  version "0.8.10"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/cnap-tech/akua/releases/download/v#{version}/akua-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "b7b36d4477466e0303aaa11ace346d96ad1da7ca543e7cfbf384d680289584f7"
+      sha256 "f79ace21790965433df8bcb3d65c5d3f40b9a48e0d4ae2c704ba271fb339f5b3"
     end
     on_intel do
       url "https://github.com/cnap-tech/akua/releases/download/v#{version}/akua-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "d246311cdb2c30b1922238826faa727f18205efa9fdf5b824fc07d8a63d2e369"
+      sha256 "f24974cda157f1a9b9a7ce4d5e7d268f0fd092340208e00ea613cf05021702ef"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/cnap-tech/akua/releases/download/v#{version}/akua-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9e3bea7de699262c22a9854531967ac71e9b051208b3ab2028533ea69a110ff6"
+      sha256 "ec4e7e72481c4ec787919a7f9f9f48fbfae119b01bd37ac6c532ca0dc5ef1f81"
     end
     on_intel do
       url "https://github.com/cnap-tech/akua/releases/download/v#{version}/akua-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "a350c69c58605492d0eabe0c570f453f98d3016a8d0a58d75aab65fb89ea8ef4"
+      sha256 "5371ea6008961921f20725c633d61a972d034653a2d557faaa851c395ea2b1e6"
     end
   end
 
